4dcd5da2a1150f947766815776ea153aa4f7ba75
pcoriente <pcoriente.desarrollos@gmail.com>
2025-08-05 13:38:22 -0600

    git ignore para permanecer el api

--- src/app/services/geo_direccionClientes/geo-clientes-direccion.service.ts
+++ src/app/services/geo_direccionClientes/geo-clientes-direccion.service.ts
@@ -5,57 +5,69 @@ import { GeoClientesDireccion } from '../../interfaces/geo_clientes-direccion';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GeoClientesDireccionService {
   private readonly API_URI = `${environment.apiUrl}/geo-clientes-direccion`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getClientesDireccion(): Observable<GeoClientesDireccion[]> {
-    return this.http.get<GeoClientesDireccion[]>(this.API_URI)
+    return this.http
+      .get<GeoClientesDireccion[]>(this.API_URI)
       .pipe(catchError(this.handleError));
   }
 
   getClienteDireccionById(id: number): Observable<GeoClientesDireccion> {
-    return this.http.get<GeoClientesDireccion>(`${this.API_URI}/${id}`)
+    return this.http
+      .get<GeoClientesDireccion>(`${this.API_URI}/${id}`)
       .pipe(catchError(this.handleError));
   }
 
-  createClienteDireccion(cliente: GeoClientesDireccion): Observable<GeoClientesDireccion> {
-    return this.http.post<GeoClientesDireccion>(this.API_URI, cliente)
+  createClienteDireccion(
+    cliente: GeoClientesDireccion
+  ): Observable<GeoClientesDireccion> {
+    return this.http
+      .post<GeoClientesDireccion>(this.API_URI, cliente)
       .pipe(catchError(this.handleError));
   }
 
-  updateClienteDireccion(id: number, cliente: GeoClientesDireccion): Observable<GeoClientesDireccion> {
-    return this.http.put<GeoClientesDireccion>(`${this.API_URI}/${id}`, cliente)
+  updateClienteDireccion(
+    id: number,
+    cliente: GeoClientesDireccion
+  ): Observable<GeoClientesDireccion> {
+    return this.http
+      .put<GeoClientesDireccion>(`${this.API_URI}/${id}`, cliente)
       .pipe(catchError(this.handleError));
   }
 
   deleteClienteDireccion(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.API_URI}/${id}`)
+    return this.http
+      .delete<void>(`${this.API_URI}/${id}`)
       .pipe(catchError(this.handleError));
   }
 
+  private handleError(error: HttpErrorResponse) {
+    // Esta comprobación funciona tanto en el navegador como en el servidor (SSR)
+    if (error.status === 0) {
+      // Ocurrió un error del lado del cliente o de la red.
+      console.error('Ocurrió un error de red o del cliente:', error.error);
+    } else {
+      // El backend retornó un código de respuesta no exitoso.
+      // El cuerpo de la respuesta puede contener pistas sobre lo que salió mal.
+      console.error(
+        `El backend retornó el código ${error.status}, ` +
+          `el cuerpo del error fue: ${JSON.stringify(error.error)}`
+      );
+    }
 
-
-
-private handleError(error: HttpErrorResponse) {
-  // Esta comprobación funciona tanto en el navegador como en el servidor (SSR)
-  if (error.status === 0) {
-    // Ocurrió un error del lado del cliente o de la red.
-    console.error('Ocurrió un error de red o del cliente:', error.error);
-  } else {
-    // El backend retornó un código de respuesta no exitoso.
-    // El cuerpo de la respuesta puede contener pistas sobre lo que salió mal.
-    console.error(
-      `El backend retornó el código ${error.status}, ` +
-      `el cuerpo del error fue: ${JSON.stringify(error.error)}`
+    // Retorna un observable con un mensaje de error legible para el usuario final.
+    // Se usa la sintaxis moderna de throwError.
+    return throwError(
+      () =>
+        new Error(
+          'Algo malo ha sucedido; por favor, inténtelo de nuevo más tarde.'
+        )
     );
   }
-  
-  // Retorna un observable con un mensaje de error legible para el usuario final.
-  // Se usa la sintaxis moderna de throwError.
-  return throwError(() => new Error('Algo malo ha sucedido; por favor, inténtelo de nuevo más tarde.'));
-}
 }
--- src/app/services/geo_rutasParadas/geo-rutas-paradas.service.ts
+++ src/app/services/geo_rutasParadas/geo-rutas-paradas.service.ts
@@ -1,54 +1,57 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../enviroments/enviroments';
-import { GeoRutasParadas } from '../../interfaces/geo_rutasParadas';
+
+import { GeoRutasParada } from '../../interfaces/geo-rutas-parada';
 import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GeoRutasParadasService {
-
   private readonly API_URI = `${environment.apiUrl}/geo-rutas-paradas`;
 
   constructor(private http: HttpClient) {}
 
-  getRutasParadasList(): Observable<GeoRutasParadas[]> {
-    return this.http.get<GeoRutasParadas[]>(this.API_URI)
-      .pipe(
-        catchError(this.handleError)
-      );
+  getRutasParadasList(): Observable<GeoRutasParada[]> {
+    return this.http
+      .get<GeoRutasParada[]>(this.API_URI)
+      .pipe(catchError(this.handleError));
   }
 
-  getRutasParada(id: number): Observable<GeoRutasParadas> {
-    return this.http.get<GeoRutasParadas>(`${this.API_URI}/${id}`)
-      .pipe(
-        catchError(this.handleError)
-      );
+  getRutasParada(id: number): Observable<GeoRutasParada> {
+    return this.http
+      .get<GeoRutasParada>(`${this.API_URI}/${id}`)
+      .pipe(catchError(this.handleError));
   }
 
-  guardarRutasParada(rutaParadas: GeoRutasParadas): Observable<GeoRutasParadas> {
-    return this.http.post<GeoRutasParadas>(this.API_URI, rutaParadas)
-      .pipe(
-        catchError(this.handleError)
-      );
+  guardarRutasParada(rutaParadas: GeoRutasParada): Observable<GeoRutasParada> {
+    return this.http
+      .post<GeoRutasParada>(this.API_URI, rutaParadas)
+      .pipe(catchError(this.handleError));
   }
 
-private handleError(error: HttpErrorResponse) {
-  // Esta comprobación funciona tanto en el navegador como en el servidor (SSR)
-  if (error.status === 0) {
-    // Ocurrió un error del lado del cliente o de la red.
-    console.error('Ocurrió un error de red o del cliente:', error.error);
-  } else {
-    // El backend retornó un código de respuesta no exitoso.
-    // El cuerpo de la respuesta puede contener pistas sobre lo que salió mal.
-    console.error(
-      `El backend retornó el código ${error.status}, ` +
-      `el cuerpo del error fue: ${JSON.stringify(error.error)}`
+  private handleError(error: HttpErrorResponse) {
+    // Esta comprobación funciona tanto en el navegador como en el servidor (SSR)
+    if (error.status === 0) {
+      // Ocurrió un error del lado del cliente o de la red.
+      console.error('Ocurrió un error de red o del cliente:', error.error);
+    } else {
+      // El backend retornó un código de respuesta no exitoso.
+      // El cuerpo de la respuesta puede contener pistas sobre lo que salió mal.
+      console.error(
+        `El backend retornó el código ${error.status}, ` +
+          `el cuerpo del error fue: ${JSON.stringify(error.error)}`
+      );
+    }
+
+    // Retorna un observable con un mensaje de error legible para el usuario final.
+    // Se usa la sintaxis moderna de throwError.
+    return throwError(
+      () =>
+        new Error(
+          'Algo malo ha sucedido; por favor, inténtelo de nuevo más tarde.'
+        )
     );
   }
-  
-  // Retorna un observable con un mensaje de error legible para el usuario final.
-  // Se usa la sintaxis moderna de throwError.
-  return throwError(() => new Error('Algo malo ha sucedido; por favor, inténtelo de nuevo más tarde.'));}
 }
